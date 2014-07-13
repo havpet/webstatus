3cb57eab3146d44b184eb88f6c383e9a78bc853c
Håvard Pettersen <h@havpet.no>
2014-07-13 17:55:17 +0200

    Added 'Address' and 'Port' fields

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	
 	//Change to desired number of seconds before refresh
-	header("Refresh: 200;url='index.php'");
+	header("Refresh: 180;url='index.php'");
 	header("Cache-Control: no-cache, must-revalidate");
 	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 
@@ -18,8 +18,6 @@
 	<meta name="mobile-web-app-capable" content="yes">
 
 <link rel="stylesheet" type="text/css" href="css/styles.css"/>
-
-<?php include 'includes/timer.inc.php'; ?>
 
 
 </head>
@@ -55,24 +53,19 @@
 		
 		$allup = true;
 		
-		$counter = 0;
 		$timeout = 1;
 		$avgtime = 0;
 		
-		echo '<table><tr><td><b>Website</b></td><td><b>Response time</b></td></tr>';
+		echo '<table><tr><td><b>Name</b></td><td><b>Address</b></td><td><b>Port</b></td><td><b>Response time</b></td></tr>';
 		
 		for($i=0; $i<count($arr); $i++) {
 			$result = $s->ping($arr[$i]["address"], $arr[$i]["port"], $timeout);
 			$time = $s->measureTime($arr[$i]["address"], $arr[$i]["port"], $timeout);
 			
 			$avgtime = $avgtime + $time;
-
-			if($counter == 1) { //Number of addresses above header
-				echo "" ;  //header
-			}
 			
 			if($result) {
-				echo '<tr class="up"><td>' .$arr[$i]["name"] . ' (port ' .$arr[$i]["port"]. ') is up!</td><td>' .$time. ' ms</td></tr>';
+				echo '<tr class="up"><td>' .$arr[$i]["name"] . '</td><td>' .$arr[$i]["address"]. '</td><td> ' .$arr[$i]["port"]. '</td><td>' .$time. ' ms</td></tr>';
 				
 			}
 		
@@ -83,11 +76,9 @@
 				
 			}
 			
-			$counter++;
-			
 		}
 		
-		echo '<tr style="border-top:2px solid #999"><td>Average response time: </td><td>' . round($avgtime/count($arr), 2) . ' ms</td></tr>';
+		echo '<tr style="border-top:2px solid #999"><td>Average: </td><td></td><td></td><td>' . round($avgtime/count($arr), 2) . ' ms</td></tr>';
 		
 		//checking if all sites are running
 		if($allup) {
@@ -118,13 +109,6 @@
 	<br />
 	
 </div>
-	
-<div id="bottomtimer">
-
-	<span>Auto-refresh in </span><span id="timer"></span>
-	
-</div>
-
 
 </body>
 
